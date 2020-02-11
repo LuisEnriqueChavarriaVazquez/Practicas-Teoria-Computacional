b9eee5af5253ccff599dde162ef8f00eb47a420a
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2020-02-10 23:31:23 -0600

    Primer intento de validacion correo y CURP
    Estas son mis primera propuestas para verificar el CURP y el correo

---
+++ expresionesRegulares.py
@@ -0,0 +1,18 @@
+import re 
+regexMail = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+regexCURP = '^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$'
+
+def checkCorreo(correo):
+    if(re.match(regexMail,correo)):
+        print("Correo Valido")
+    else:
+        print("Correo Invalido")
+
+def checkCURP(CURP):
+    if(re.match(regexCURP,CURP)):
+        print("CURP Valido")
+    else:
+        print("CURP Invalido")
+
+
+
