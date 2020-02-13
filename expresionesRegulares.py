0bd3e6c926a84ff4c01e22a46db1dae86c1ed7c7
Luis Enrique <luisechvz@gmail.com>
2020-02-13 12:02:11 -0600

    Funcion para validar la URL

--- expresionesRegulares.py
+++ expresionesRegulares.py
@@ -2,7 +2,7 @@ import re
 regexMail = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
 regexCURP = '^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$'
 regexRFC ='^([A-Z]{4}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01]))'#No esta completa tenog dudas con la homoclave del RFC
-regexURL ##Voy a agregar la validacion de una URL con expresiones regulares para que halla un extra.
+regexURL = '^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?'
 
 
 
@@ -23,6 +23,12 @@ def checkRFC(RFC):
         print("\n### RFC Valido")
     else:
         print("\n### RFC Invalido")
+    
+def checkURL(URL):
+    if(re.match(regexURL,URL)):
+        print("\n## URL Valido")
+    else:
+        print("\n## URL No valido")
 
  
 def pedirNumeroEntero():
@@ -51,7 +57,8 @@ while not salir:
         [1] Validar EMAIL
         [2] Validar CURP
         [3] Validar RFC
-        [4] Salir
+        [4] Validar URL
+        [5] Salir
         
     """)
  
@@ -67,6 +74,8 @@ while not salir:
         rfc_value = str(input("Escriba algun RFC para validar == "))
         checkRFC(rfc_value)
     elif opcion == 4:
+        url_value = str(input("Escriba el URL que desea verificar == "))
+    elif opcion == 5:
         salir = True
     else:
         print ("Introduce un numero entre 1 y 3")
